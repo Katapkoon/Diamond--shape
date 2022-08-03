6843f67e5b4ceeed3f7ccb238856f891439eb06a
Katapkoon <katapat80@gmail.com>
2022-08-03 09:23:16 +0700

    9.22 AM 8/3/2022

--- Diamond-shape.c
+++ Diamond-shape.c
@@ -1,34 +1,29 @@
 #include <stdio.h>
 int main()
 {
-  int size,size_input,symbol;
+  int size,size_input,symbol,space;
 
   scanf("%d",&size_input);
   
   for(size = 1 ; size <= size_input; size++)
   {
-    for(int space = 0; space < size_input - size; space++)
+    for(space = 1; space <= size_input - size; space++)
     {
-      printf("-");
+      printf(" ");
     }
-    for(symbol = 0; symbol < 2*(size) - 1; symbol++)
+    for(symbol = 1; symbol <= 2*(size) - 1; symbol++)
     {
       printf("*");
-    }
-    
-    for(int space = 0;space < size_input - size; space++)
-    {
-      printf("-");
     }
     printf("\n");
   }
   for(size = 1; size <= size_input - 1; size++)
   {
-    for(int space  = 1; space <= size; space++)
+    for(space  = 1; space <= size; space++)
     {
-      printf("-");
+      printf(" ");
     }
-    for(symbol = 1; symbol < 2*(size_input - size) - 1; symbol++)
+    for(symbol = 1; symbol <= 2*(size_input - size) - 1; symbol++)
     {
       printf("*");
     }
